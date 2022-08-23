84659fdd1988d89737f79ee3fb2fb491a604958c
akm-ksk <akmmst0496@gmail.com>
2022-08-23 21:57:35 +0900

    Header Footer ResetCSS

--- reaxt-ts/src/App.tsx
+++ reaxt-ts/src/App.tsx
@@ -6,11 +6,14 @@ import {ReportDetail} from "./report/pages/ReportDetail";
 import {CategoryView} from "./report/pages/CategoryView";
 import {TopPage} from "./report/pages/TopPage";
 import {NotFoundPage} from "./report/pages/NotFoundPage";
+import {Header} from "./report/components/Header";
+import {Footer} from "./report/components/Footer";
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
+                <Header/>
                 <Routes>
                     <Route path="/" element={<TopPage/>}/>
                     <Route path="/report/" element={<ReportTop/>}/>
@@ -18,6 +21,7 @@ function App() {
                     <Route path="/report/category/:cat" element={<CategoryView/>}/>
                     <Route path="*" element={<NotFoundPage/>}/>
                 </Routes>
+                <Footer/>
             </BrowserRouter>
         </div>
     );
--- reaxt-ts/src/report/components/Header.tsx
+++ reaxt-ts/src/report/components/Header.tsx
@@ -2,15 +2,17 @@ import {Link} from "react-router-dom";
 
 export const Header = () => {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">TOP</Link>
-                </li>
-                <li>
-                    <Link to="/report">Report</Link>
-                </li>
-            </ul>
-        </nav>
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">TOP</Link>
+                    </li>
+                    <li>
+                        <Link to="/report">Report</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
